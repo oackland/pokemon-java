fe100fdde51a57317dd2aa45042b7fbb38ce6c8a
oackland <oackland.toro@outlook.com>
2023-09-06 14:18:52 -0600

    java api is working well

--- weather_api/js/main.js
+++ weather_api/js/main.js
@@ -1,57 +1,65 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Variables and Constants
     const apiKey = "38d147031dc4e997fc0b84ac609f3f86";
-    const main = document.getElementById('main');
-    const form = document.getElementById('form');
-    const search = document.getElementById('search');
-    const toggleButton = document.getElementById('toggle');
+    const mainElement = document.getElementById('main');
+    const formElement = document.getElementById('form');
+    const searchElement = document.getElementById('search');
+    const toggleButtonElement = document.getElementById('toggle');
     let isCelsius = true;
 
-    function KtoC(K) {
+    let currentWeatherData = null;
+    let currentForecastData = null;
+
+    // Utility Functions
+    function toCelsius(K) {
         return Math.floor(K - 273.15);
     }
 
-    function KtoF(K) {
+    function toFahrenheit(K) {
         return Math.floor((K - 273.15) * 9 / 5 + 32);
     }
 
-    const weatherUrl = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    const forecastUrl = (city) => `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    // API URL Builders
+    const buildWeatherUrl = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const buildForecastUrl = (city) => `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
-    async function getWeatherByLocation(city) {
-        const resp = await fetch(weatherUrl(city));
-        const respData = await resp.json();
-        addWeatherToPage(respData);
+    // Fetch and Display Functions
+    async function fetchAndDisplayWeather(city) {
+        const response = await fetch(buildWeatherUrl(city));
+        const data = await response.json();
+        currentWeatherData = data;
+        addWeatherToPage(data);
+    }
 
-        const respForecast = await fetch(forecastUrl(city));
-        const respForecastData = await respForecast.json();
-        addForecastToPage(respForecastData);
+    async function fetchAndDisplayForecast(city) {
+        const response = await fetch(buildForecastUrl(city));
+        const data = await response.json();
+        currentForecastData = data;
+        addForecastToPage(data);
     }
 
     function addWeatherToPage(data) {
-        const temp = isCelsius ? KtoC(data.main.temp) : KtoF(data.main.temp);
-        const unit = isCelsius ? "°C" : "°F";
+        const tempUnit = isCelsius ? "°C" : "°F";
+        const tempValue = isCelsius ? toCelsius(data.main.temp) : toFahrenheit(data.main.temp);
 
         const date = new Date();
         const time = `${date.getHours()}:${date.getMinutes()}`;
 
-        // Add additional weather information here, like pollution, UV index, etc.
+        const weatherDiv = document.createElement('div');
+        weatherDiv.classList.add('weather');
+        weatherDiv.innerHTML = `
+            <div id="current-time-date">
+                <div>Date: <span id="current-date">${date.toDateString()}</span></div>
+                <div>Time: <span id="current-time">${time}</span></div>
+            </div>
+            <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
+            ${tempValue}${tempUnit}</h2>
+            <small>${data.weather[0].main}</small>
+        `;
 
-        const weather = document.createElement('div');
-        weather.classList.add('weather');
-        weather.innerHTML = `
-        <div id="current-time-date">
-            <div>Date: <span id="current-date">${date.toDateString()}</span></div>
-            <div>Time: <span id="current-time">${time}</span></div>
-        </div>
-        <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
-        ${temp}${unit}</h2>
-        <small>${data.weather[0].main}</small>`;
-
-        main.innerHTML = "";
-        main.appendChild(weather);
+        mainElement.innerHTML = "";
+        mainElement.appendChild(weatherDiv);
     }
-
 
     function addForecastToPage(data) {
         const forecastContainer = document.createElement('div');
@@ -59,27 +67,46 @@ document.addEventListener('DOMContentLoaded', function () {
 
         for (let i = 0; i < data.list.length; i += 8) {
             const dayData = data.list[i];
-            const temp = KtoC(dayData.main.temp);
+            const tempValue = isCelsius ? toCelsius(dayData.main.temp) : toFahrenheit(dayData.main.temp);
+            const tempUnit = isCelsius ? "°C" : "°F";
 
-            const day = document.createElement('div');
-            day.classList.add('day-forecast');
-            day.innerHTML = `
-                <h3>Day ${(i / 8) + 1}</h3>
-                <div class="temp">Temperature: <span class="temp">${temp}°C</span></div>
+            const date = new Date(dayData.dt * 1000);
+            const formattedDate = date.toISOString().split('T')[0];
+
+            const dayDiv = document.createElement('div');
+            dayDiv.classList.add('day-forecast');
+            dayDiv.innerHTML = `
+                <h3>${formattedDate}</h3>
+                <div class="temp">Temperature: <span class="temp">${tempValue}${tempUnit}</span></div>
                 <div class="description">Description: <span class="description">${dayData.weather[0].main}</span></div>
             `;
-            forecastContainer.appendChild(day);
+            forecastContainer.appendChild(dayDiv);
         }
 
-        main.appendChild(forecastContainer);
+        mainElement.appendChild(forecastContainer);
     }
 
+    function refreshDisplay() {
+        if (currentWeatherData) {
+            addWeatherToPage(currentWeatherData);
+        }
+        if (currentForecastData) {
+            addForecastToPage(currentForecastData);
+        }
+    }
 
-    form.addEventListener('submit', (e) => {
+    // Event Handlers
+    formElement.addEventListener('submit', (e) => {
         e.preventDefault();
-        const city = search.value;
+        const city = searchElement.value;
         if (city) {
-            getWeatherByLocation(city);
+            fetchAndDisplayWeather(city);
+            fetchAndDisplayForecast(city);
         }
     });
-})
+
+    toggleButtonElement.addEventListener('click', () => {
+        isCelsius = !isCelsius;
+        refreshDisplay();
+    });
+});
